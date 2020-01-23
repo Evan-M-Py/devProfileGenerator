f9a771b4c3b3b3a3ad585972ce82ee596249f7ab
Evan Pylar <evan.m.pylar@gmail.com>
2020-01-23 16:28:21 -0700

    project foundation and initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const github = require('github-api');
+const axios = require('axios');
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "userName"
+      },    
+    {
+      type: "checkbox",
+      message: "choose your theme color?",
+      name: "colorChoice",
+      choices: [
+        "dark blue", 
+        "light blue", 
+        "burnt orange", 
+        "monochrome"
+      ]
+    }
+  ]).then((data) => {
+    const gh = new github({
+        username: 'Evan-M-Py',
+        password: 'Phishin95!'
+     });  
+
+    const userGrab = gh.getProfile(data.userName)
+    console.log(userGrab)
+  })

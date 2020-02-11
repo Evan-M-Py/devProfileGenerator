25e2211b70ea8d1cb5b683dce097db9ac8509a59
Evan Pylar <evan.m.pylar@gmail.com>
2020-02-10 21:31:24 -0700

    functional dev profile generator in pdf

--- index.js
+++ index.js
@@ -2,36 +2,12 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const GitHub = require('github-api');
 const axios = require('axios');
-
-function init() {
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "What is your Github username?",
-      name: "userName"
-    },
-    {
-      type: "checkbox",
-      message: "choose your theme color?",
-      name: "color",
-      choices: [
-        { name: 'green', value: colors.green, short: 'green' },
-        { name: 'blue', value: colors.blue, short: 'blue' },
-        { name: 'pink', value: colors.pink, short: 'pink' },
-        { name: 'red', value: colors.red, short: 'red' }]
-    }
-  ]).then(async function (data) {
-    const profRes =  generalQuery(data)
-    const { profileIMG, gitName, gitLink, gitBio } = profRes
-
-    const specificRes =  await specificQueries(data);
-    console.log(specificRes)
-  })};
+const pdf = require('html-pdf');
   
 
-function generalQuery(data) {
+async function generalQuery(data) {
   const queryUrl = `https://api.github.com/users/${data.userName}`;
-  const ajax =  axios.get(queryUrl).then(function (res) {
+  const ajax = await axios.get(queryUrl).then(function (res) {
     const gitUserInfo = {
       profileIMG: res.data.avatar_url,
       gitName: res.data.login,
@@ -75,8 +51,8 @@ async function specificQueries(data) {
 
 
 
-function writeToFile(data) {
-  fs.writeFile('test.html', data, (err) => {
+function writeToFile(html) {
+  fs.writeFile('devOne.html', html, (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   })
@@ -115,8 +91,8 @@ const colors = {
   }
 };
 
-function generateHTML(data) {
-  return `<!DOCTYPE html>
+async function generateHTML(data, dataTwo, dataThree) {
+  const theHtml =  `<!DOCTYPE html>
   <html lang="en">
      <head>
         <meta charset="UTF-8" />
@@ -125,6 +101,43 @@ function generateHTML(data) {
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
         <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
         <title>Document</title>
+      </head>
+        <body>
+        <div class="wrapper">
+          <div class="photo-header">
+            <p><img id="prof-img" src="${dataTwo.profileIMG}"></img></p>
+            <h1>Hello World!</h1>
+            <h1>My name is ${dataTwo.gitName}!</h1>
+            <div>
+              <h4><img src="https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_location_on_48px-512.png" width="30" height="30"><a ></a></h4>
+              <h4><img src="https://image.flaticon.com/icons/png/512/25/25231.png" width="30" height="30"><a href=>GitHub</a></h4>
+              <h4><img src="https://www.freeiconspng.com/uploads/blogger-logo-icon-png-28.png" width="30" height="30"><a>Blog</a></h4>
+            </div>
+          </div>
+        </div>
+        <h2 id="bio">${dataTwo.gitBio}</h2>
+        <div class="container">
+          <div class='row'>
+            <div class='col'>
+              <div class="card">
+              <h3>Public Repositories:</h3>${dataThree.repoNum}
+              </div>
+              <div class="card">
+              <h3>GitHub Stars</h3>${dataThree.starredNum}
+              </div>
+            </div>
+            <div class='col'>
+              <div class="card">
+              <h3>Followers</h3>${dataThree.followerNum}
+              </div>
+              <div class="card">
+              <h3>Following</h3>${dataThree.followingNum}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="wrapper"></div>
+        </body>
         <style>
             @page {
               margin: 0;
@@ -139,11 +152,12 @@ function generateHTML(data) {
            margin: 0;
            }
            html, body, .wrapper {
-           height: 100%;
+           height: 85%;
            }
            .wrapper {
-           background-color: ${colors[data.color].wrapperBackground};
+           background-color: ${data.color[0].wrapperBackground};
            padding-top: 100px;
+           text-align:center;
            }
            body {
            background-color: white;
@@ -184,19 +198,19 @@ function generateHTML(data) {
            display: flex;
            justify-content: center;
            flex-wrap: wrap;
-           background-color: ${colors[data.color].headerBackground};
-           color: ${colors[data.color].headerColor};
+           background-color: ${data.color[0].headerBackground};
+           color: ${data.color[0].headerColor};
            padding: 10px;
            width: 95%;
            border-radius: 6px;
            }
-           .photo-header img {
-           width: 250px;
-           height: 250px;
+           #prof-img {
+           width: 300px;
+           height: 300px;
            border-radius: 50%;
            object-fit: cover;
            margin-top: -75px;
-           border: 6px solid ${colors[data.color].photoBorderColor};
+           border: 6px solid ${data.color[0].photoBorderColor};
            box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
            }
            .photo-header h1, .photo-header h2 {
@@ -206,117 +220,94 @@ function generateHTML(data) {
            .photo-header h1 {
            margin-top: 10px;
            }
-           .links-nav {
-           width: 100%;
-           text-align: center;
-           padding: 20px 0;
-           font-size: 1.1em;
-           }
-           .nav-link {
-           display: inline-block;
-           margin: 5px 10px;
-           }
-           .workExp-date {
-           font-style: italic;
-           font-size: .7em;
-           text-align: right;
-           margin-top: 10px;
-           }
            .container {
            padding: 50px;
            padding-left: 100px;
            padding-right: 100px;
+           text-align:center;
            }
-  
            .row {
-             display: flex;
-             flex-wrap: wrap;
-             justify-content: space-between;
-             margin-top: 20px;
-             margin-bottom: 20px;
-           }
-  
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            margin-top: 20px;
+            margin-bottom: 20px;
+          }
            .card {
              padding: 20px;
              border-radius: 6px;
-             background-color: ${colors[data.color].headerBackground};
-             color: ${colors[data.color].headerColor};
+             background-color: ${data.color[0].headerBackground};
+             color: ${data.color[0].headerColor};
              margin: 20px;
            }
-           
            .col {
            flex: 1;
            text-align: center;
            }
-  
+           #bio {
+            margin-top: 5%;
+            text-align: center;
+            padding-top: 5%;
+           }
            a, a:hover {
            text-decoration: none;
            color: inherit;
            font-weight: bold;
            }
-  
            @media print { 
             body { 
               zoom: .75; 
             } 
            }
         </style>
-      </head>
-  
-      <body>
-          <div class="wrapper">
-              <div class="photo-header">
-                  <div class="photo-header"><img src="" alt=""></div>
-                  <h1 id="hello"></h1>
-                  <h2 id="name intro"></h2>
-                  <h3 id="education"></h3>
-                  <div id="linkage">
-                      <a href=""></a>
-                      <a href=""></a>
-                      <a href=""></a>
-                      <a href=""></a>
-                  </div>
-              </div>
-  
-              <div class="container">
-                  <div class="row">
-                      <h2>I write code and save lives!</h2>                   
-                  </div>
-              </div>
-  
-              <div class="container">
-                  <div class="row">
-                      <div class="col">
-                          <div class="card">Public Repositories
-                              <h5></h5>
-                          </div>
-                          <div class="card">Github Stars
-                              <h5></h5>
-                          </div>
-                      </div>
-                      <div class="col">
-                          <div class="card">Followers
-                              <h5></h5>
-                          </div>
-                          <div class="card">Following
-                              <h5></h5>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-  
-              <div class="container">
-                  <div class="row">
-                      <div class="card"></div>
-                  </div>
-              </div>
-              
-              
-          </div>
-          
-      </body>
-      </html>`
+        </html>`
+  return theHtml
 }
+
+function init() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your Github username?",
+      name: "userName"
+    },
+    {
+      type: "checkbox",
+      message: "choose your theme color?",
+      name: "color",
+      choices: [
+        { name: 'green', value: colors.green, short: 'green' },
+        { name: 'blue', value: colors.blue, short: 'blue' },
+        { name: 'pink', value: colors.pink, short: 'pink' },
+        { name: 'red', value: colors.red, short: 'red' }]
+    }
+  ]).then(async function (data) {
+    const color = await data.color;
+    const profRes = await generalQuery(data)
+    const { profileIMG, gitName, gitLink, gitBio } = profRes
+
+    const specificRes =  await specificQueries(data);
+  
+
+    const genHtml = await generateHTML(data, profRes, specificRes)
+
+    await writeToFile(genHtml);
+    const htmlPage = await fs.readFileSync('./devOne.html', 'utf8')
+    const options = { format: 'letter'};;
+
+    pdf.create(genHtml).toFile('./profile.pdf', function (err, res) {
+      if (err) return console.log('ERROR INDEX.JS LINE 24');
+      console.log('Success!!!!');
+  });
+
+
+
+
+    
+    
+    
+
+  })};
 init();
 
 
